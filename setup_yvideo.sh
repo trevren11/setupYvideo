bd75a4e7353883cb1aaa55cfb621808b8443e229
David Ostler <david.ostler001@gmail.com>
2017-02-17 09:54:58 -0700

    initial (working) commit

---
+++ setup_yvideo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "1. Making Directories..."
+cd ~
+mkdir -p Docker/ && cd Docker
+
+echo
+echo "2. Cloning Dependencies..."
+git clone https://github.com/dro248/ayamelDBDockerfile
+git clone https://github.com/dro248/AyamelDockerfile
+git clone https://github.com/dro248/runAyamel
+
+echo
+echo "3. Getting secret..."
+scp arclite@sartre4.byu.edu:~/application.conf ./AyamelDockerfile
+
+echo
+echo "4. Making space for database..."
+sudo service mysql stop
+
+echo
+echo "5. Creating Database & App..."
+cd runAyamel
+sudo docker-compose up
