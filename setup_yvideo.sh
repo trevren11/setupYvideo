05c438c1da4e6611710d13a631c81a55cbddfe2a
Trevor Renshaw <trevren11@gmail.com>
2017-04-12 20:32:10 -0600

    Use encrypted file

--- setup_yvideo.sh
+++ setup_yvideo.sh
@@ -6,16 +6,15 @@ mkdir -p Docker/ && cd Docker
 
 echo
 echo "2. Cloning Dependencies..."
-git clone https://github.com/dro248/ayamelDBDockerfile
+git clone https://github.com/trevren11/ayamelDBDockerfile
 git clone https://github.com/dro248/AyamelDockerfile
 git clone https://github.com/dro248/runAyamel
 
 echo
 echo "3. Getting secret..."
 
-echo $TEST_CRED
-sudo apt-get install sshpass
-sshpass -p $APPLICATION_CREDENTIALS scp arclite@sartre4.byu.edu:~/application.conf ./AyamelDockerfile
+mv BYU-ODH/Ayamel/conf/application.conf ./AyamelDockerfile
+ls -al 
 
 echo
 echo "4. Making space for database..."
